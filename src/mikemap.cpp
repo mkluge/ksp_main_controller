5d0d0f00901e40657972197d0c64c92fd72790d1
Michael Kluge <vollseil@gmail.com>
2019-11-03 18:30:45 +0100

    added some tests

--- src/mikemap.cpp
+++ src/mikemap.cpp
@@ -1,23 +1,61 @@
 #include "mikemap.h"
-#include <ArduinoUnit.h>
 
-/*
-test(map)
+#ifdef TEST_MIKEMAP
+#include <assert.h>
+
+#define DEFAULT_TEST_VALUE 99
+
+int main(int argc, char* argv[])
 {
   MikeMap mm;
-  assertEqual(mm.get_len(), 0);
-  for( int i=0; i<200; i++)
+  MAP_KEY_TYPE key=DEFAULT_TEST_VALUE;
+  MAP_VALUE_TYPE value=DEFAULT_TEST_VALUE;
+  
+  assert(mm.get_len()==0);
+  for( int i=0; i<MAX_MAP_LEN-4; i++)
     mm.set(i,i);
-  assertEqual(mm.get_len(), MAX_MAP_LEN);
+  assert(mm.get_len()==MAX_MAP_LEN-4);
   mm.del(1);
-  assertEqual(mm.get_len(), MAX_MAP_LEN-1);
-  assertEqual(mm.get(4), 4);
-  assertEqual(mm.has(1), false);
-  assertEqual(mm.has(2), true);
+  assert(mm.get_len()==MAX_MAP_LEN-5);
+  assert(mm.get(4)==4);
+  assert(mm.has(1)==false);
+  assert(mm.has(2)==true);
+  mm.get_at( MAX_MAP_LEN+4, &key, &value);
+  assert( key==DEFAULT_TEST_VALUE);
+  assert( value==DEFAULT_TEST_VALUE);
+  mm.get_at( 3, &key, &value);
+  assert( key==4);
+  assert( value==4);
   mm.clear();
-  assertEqual(mm.get_len(), 0);
-}
-*/
+  key=DEFAULT_TEST_VALUE;
+  value=DEFAULT_TEST_VALUE;
+  assert(mm.get_len()==0);
+  for( int i=0; i<MAX_MAP_LEN-4; i++)
+    mm.set(i,i);
+  assert(mm.get_len()==MAX_MAP_LEN-4);
+  mm.del(1);
+  assert(mm.get_len()==MAX_MAP_LEN-5);
+  assert(mm.get(4)==4);
+  assert(mm.get(10)==10);
+  assert(mm.has(1)==false);
+  assert(mm.has(2)==true);
+  mm.get_at( MAX_MAP_LEN+4, &key, &value);
+  assert( key==DEFAULT_TEST_VALUE);
+  assert( value==DEFAULT_TEST_VALUE);
+  mm.get_at( 3, &key, &value);
+  assert( key==4);
+  assert( value==4);
+  mm.del(2);
+  mm.del(5);
+  mm.set(2,20);
+  assert(mm.has(2)==true);
+  assert(mm.get_len()==MAX_MAP_LEN-6);
+  mm.get_at( MAX_MAP_LEN-7, &key, &value);
+  assert( key==2);
+  assert( value==20);
+  assert(mm.has(5)==false);
+ }
+#endif
 
 MikeMap::MikeMap()
 {
